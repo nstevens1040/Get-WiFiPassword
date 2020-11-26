38e5dc60d8197b00d7bf9cda31b5e9211ef3c158
Nicholas Stevens <nstevens@nanick.org>
2020-11-26 13:19:29 -0600

    Update Get-WifiPassword.ps1

--- Get-WifiPassword.ps1
+++ Get-WifiPassword.ps1
@@ -4,5 +4,8 @@ Function Get-WifiPw
     Param(
         [string]$Wifi_SSID
     )
+    if(!$Wifi_SSID){
+        $Wifi_SSID = (Get-NetConnectionProfile).Name
+    }
     return [Regex]::new("(\s*)").Replace(@(cmd /c "netsh wlan show profile `"$($Wifi_SSID)`" key=clear 2>&1").Where({$_ -match 'Key Content'})[0].Split(':')[1],'')
 }
