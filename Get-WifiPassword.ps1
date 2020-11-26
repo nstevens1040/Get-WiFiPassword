ffba8f0e437d253c94eae35f5e4eda7b5d527b94
Nicholas Stevens <nstevens@nanick.org>
2020-11-26 13:35:32 -0600

    Update Get-WifiPassword.ps1

--- Get-WifiPassword.ps1
+++ Get-WifiPassword.ps1
@@ -7,5 +7,6 @@ Function Get-WifiPw
     if(!$Wifi_SSID){
         $Wifi_SSID = (Get-NetConnectionProfile).Name
     }
-    return [Regex]::new("(\s*)").Replace(@(cmd /c "netsh wlan show profile `"$($Wifi_SSID)`" key=clear 2>&1").Where({$_ -match 'Key Content'})[0].Split(':')[1],'')
+    $pw = [Regex]::new("(\s*)").Replace(@(cmd /c "netsh wlan show profile `"$($Wifi_SSID)`" key=clear 2>&1").Where({$_ -match 'Key Content'})[0].Split(':')[1],'')
+    write-host "$($Wifi_SSID)" -ForeGroundColor Yellow -NoNewLine; @(0..("$($Wifi_SSID)".Legnth + 8)).ForEach({ write-host " " -NoNewLine }); write-host "$($pw)" -ForeGroundColor Green
 }
